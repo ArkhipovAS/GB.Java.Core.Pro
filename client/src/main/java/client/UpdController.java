8ad196a37bb42b20bdde1bfd7c08ab93e98112ff
ArkhipovAS <arhipov.sasha@gmail.com>
2020-05-08 13:29:16 +0700

    add hw2 java core pro add registration in the DB add function change nickname

---
+++ client/src/main/java/client/UpdController.java
@@ -0,0 +1,34 @@
+package client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class UpdController {
+    Controller controller;
+
+    @FXML
+    public TextField loginField;
+    @FXML
+    public PasswordField passwordField1;
+    @FXML
+    public PasswordField passwordField2;
+    @FXML
+    public TextField nickField;
+
+    public void tryToUpd() {
+        String login = loginField.getText().trim();
+        String password1 = passwordField1.getText().trim();
+        String nickname = nickField.getText().trim();
+//        if (!password1.equals(password2)) {
+//            controller.textArea.appendText("Пароли не совпадают");
+//        }
+        //
+
+        controller.tryUpdate(login, password1, nickname);
+
+        ((Stage) loginField.getScene().getWindow()).close();
+    }
+}
---
+++ server/src/main/java/server/AuthService.java
@@ -0,0 +1,8 @@
+package server;
+
+public interface AuthService {
+    String getNicknameByLoginAndPassword(String login, String password);
+    boolean registration(String login, String password, String nickname);
+
+    boolean update(String login, String password, String nickname);
+}
